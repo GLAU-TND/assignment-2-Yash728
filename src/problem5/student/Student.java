aece19316c71eec173bee78441bd64a812da6c45
Yash728 <61221867+Yash728@users.noreply.github.com>
2020-03-27 19:08:01 +0530

    Defined node class using the object of student from problem 5. Defined a priority queue to store in ascending order. Defined the main funtion.

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -7,4 +7,21 @@
 package problem5.student;
 // to store student information and properties
 public class Student {
+    String Name;
+    int roll;
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
 }
